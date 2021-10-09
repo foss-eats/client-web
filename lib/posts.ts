df4dcdb611e970b91bce96996b8fa2834b4cf776
Philipp Dargel <chisui.pd@gmail.com>
2021-10-09 20:17:22 +0200

    refactore to typescript

--- lib/posts.ts
+++ lib/posts.ts
@@ -6,10 +6,14 @@ import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
+type Post = {
+  id: string,
+  [key: string]: any
+}
 export function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map(fileName => {
+  const allPostsData: Post[] = fileNames.map(fileName => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '')
 
